5f817db534281417e59d43060b42c9c3d8f0d9ff
Renee Sabdani <95228207+rennnn09@users.noreply.github.com>
2022-06-08 19:41:51 +0800

    Update MathOperators.java

--- javaProject1/src/javaProject1/MathOperators.java
+++ javaProject1/src/javaProject1/MathOperators.java
@@ -1,5 +1,57 @@
 package javaProject1;
 
+import java.util.Scanner;
 public class MathOperators {
 
-}
+		public static void main(String[] args) {
+		   int result;
+	    Scanner scan = new Scanner(System.in);
+	      
+	    try
+	    {
+	       System.out.print("Enter a number: ");
+	       int number1 = Integer.parseInt(scan.nextLine());
+	       
+	       System.out.print("Enter a Mathematical Operator: ");
+	       String MO = scan.next();
+	      
+	       System.out.print("Enter a number: ");
+	       int number2 = scan.nextInt();
+	          switch (MO) {
+	          case "+":
+	              result = number1 + number2;
+	              System.out.println(number1 + " + " + number2 + " = " + result);
+	              break;
+
+	            case "-":
+	              result = number1 - number2;
+	              System.out.println(number1 + " - " + number2 + " = " + result);
+	              break;
+
+	            case "*":
+	              result = number1 * number2;
+	              System.out.println(number1 + " * " + number2 + " = " + result);
+	              break;
+
+	            case "/":
+	              result = number1 / number2;
+	              System.out.println(number1 + " / " + number2 + " = " + result);
+	              break;
+	          
+	            case "%":
+	                result = number1 % number2;
+	                System.out.println(number1 + " % " + number2 + " = " + result);
+	                break;
+	          }
+	       
+	    }
+	    catch (ArithmeticException ex)
+	    { 
+	    	System.out.println("Arithmetic Exception occurred.");
+	    }
+	    catch (NumberFormatException ex)
+	    { 
+	       System.out.println("Number Format Exception occurred.");
+	    }
+	 }
+	}
